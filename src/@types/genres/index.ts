cd1dd94caad1f2165bbf78f2a46d0fb3ac388c1a
Nick G <nicholasgalante1997@gmail.com>
2022-08-30 17:01:11 -0600

    am a duck, forgot to lint

--- src/@types/genres/index.ts
+++ src/@types/genres/index.ts
@@ -1,9 +1,39 @@
-export type Genre = 'genesis' | 'loathing' | 'beauty' | 'governance' | 'media' | 'shadow' | 'rock' | 'glory' | 'drugs' | 'loss' | 'life' | 'colorado' | 'no-name' | 'philosophy/violence' | 'cthulhu' | 'catch-22' | 'economics' | 'reminiscence';
+export type Genre =
+  | 'genesis'
+  | 'loathing'
+  | 'beauty'
+  | 'governance'
+  | 'media'
+  | 'shadow'
+  | 'rock'
+  | 'glory'
+  | 'drugs'
+  | 'loss'
+  | 'life'
+  | 'colorado'
+  | 'no-name'
+  | 'philosophy/violence'
+  | 'cthulhu'
+  | 'catch-22'
+  | 'economics'
+  | 'reminiscence';
 export const ORIGIN_ARRAY: readonly Genre[] = ['genesis', 'beauty'] as const;
-export const GOVERNANCE_ARRAY: readonly Genre[] = ['governance', 'media', 'shadow', 'no-name', "philosophy/violence"] as const;
-export const CULT_ARRAY: readonly Genre[] = ['loathing', 'no-name', 'philosophy/violence', 'cthulhu', 'catch-22'] as const;
+export const GOVERNANCE_ARRAY: readonly Genre[] = [
+  'governance',
+  'media',
+  'shadow',
+  'no-name',
+  'philosophy/violence'
+] as const;
+export const CULT_ARRAY: readonly Genre[] = [
+  'loathing',
+  'no-name',
+  'philosophy/violence',
+  'cthulhu',
+  'catch-22'
+] as const;
 
 const ICON_MAP = {
-    'genesis': '',
-    'cthulhu': ''
+  genesis: '',
+  cthulhu: ''
 };
