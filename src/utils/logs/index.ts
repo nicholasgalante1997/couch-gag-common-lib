7bb8ac1ebd831e4a58db5ffb7b047309ca6918f5
Nick G <nicholasgalante1997@gmail.com>
2022-11-03 17:54:18 -0600

    removed chalk dep, updated log fn

--- src/utils/logs/index.ts
+++ src/utils/logs/index.ts
@@ -1,18 +1,5 @@
-import { LogColorMapType, LogLevel } from '../../@types';
-import chalk from 'chalk';
-
-const getLogColorMap: LogColorMapType = {
-  info: (s: string) => chalk.bgCyan(s),
-  debug: (s: string) => chalk.bgMagenta(s),
-  warn: (s: string) => chalk.bgYellow(s),
-  error: (s: string) => chalk.bgRed(s),
-  bright: (s: string) => chalk.bgGreenBright(s)
-} as const;
+import { LogLevel } from '../../@types';
 
 export function log(logLevel: LogLevel, msg: string) {
-  console.log(
-    getLogColorMap[logLevel](
-      `${logLevel}: msg={${msg}} emitTime={${new Date().toISOString()}}`
-    )
-  );
+  console.log(`[${Date.now()}][${logLevel.toUpperCase()}] ${msg}\n`);
 }
